158d0e16af566dd55b6204012a65771d390b833f
nurrbb <enurb320@gmail.com>
2025-12-04 15:21:59 +0300

    feat: EJS Template Engine Integration and Static File Management

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
+const app = express();
+
+app.set("view engine","ejs");
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+
+app.use(express.static(`public`));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.render('index')
+});
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+app.get('/add', (req, res) => {
+  res.render('add')
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
