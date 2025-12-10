b30712385541dd54f8b6a1337ffa3e2d7bfbfdc6
nurrbb <enurb320@gmail.com>
2025-12-10 11:34:18 +0300

    Integrate MongoDB photos and update add form

--- app.js
+++ app.js
@@ -1,33 +1,56 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
 
+//connect DB
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/pcat-test-db'
+);
+
+//TEMPLATE ENGINE
 app.set("view engine","ejs");
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware Log 1');
-  next();
-};
+// const myLogger = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next();
+// };
 
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware Log 1');
-  next();
-};
+// const myLogger2 = (req, res, next) => {
+//   console.log('Middleware Log 1');
+//   next();
+// };
 
+//MIDDLEWARES
 app.use(express.static(`public`));
-app.use(myLogger);
-app.use(myLogger2);
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.render('index')
+// app.use(myLogger);
+// app.use(myLogger2);
+
+//ROUTES
+
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index',{photos})
 });
+
 app.get('/about', (req, res) => {
   res.render('about')
 });
+
 app.get('/add', (req, res) => {
   res.render('add')
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
 });
 
 const port = 3000;
